67e6f8c0b52d585075753c8fd4380cad4bc810e7
Sara <sara.perez@enti.cat>
2022-03-19 19:49:25 +0100

    Sonidos Footsteps
    Añadido los sonidos de footstep del player más script para controlar los sonidos en las animaciones del player. Tambien se han añadido tags y algunos elementos del nivel DIA tienen tangs para poder comprobar si los sonidos de las pisadas estan bien. Los footsteps del player estan WIP.

---
+++ Assets/Scripts/Player/Player_Sounds.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Player_Sounds : MonoBehaviour
+{
+    [Header("Pasos")]
+    [SerializeField]
+    AudioClip[] footStepGrassClip;
+    [SerializeField]
+    AudioClip[] footStepConcreteClip;
+    [SerializeField]
+    AudioClip[] footStepSandClip;
+    [SerializeField]
+    AudioClip[] footStepWaterClip;
+    [SerializeField]
+    AudioClip[] footStepWoodClip;
+    [SerializeField]
+    AudioClip[] footStepMetalicClip;
+    [SerializeField]
+    AudioClip[] footStepGroundClip;
+    
+    [Header("Acciones")]
+    public AudioClip[] jumpClip;
+    public AudioClip[] pushClip;
+    public AudioClip[] painClip;
+
+    public string floorMaterial;
+
+    
+    
+
+    public void PlayFootStepSound()
+    {
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.volume = audioSource.volume;
+        audioSource.pitch = audioSource.pitch;
+
+        AudioClip[] footstepClip = footStepGroundClip;
+
+        switch (floorMaterial)
+        {
+
+            case "Grass": //Hierba
+                footstepClip = footStepGrassClip;
+                break;
+            case "Ground": //Tierra
+                footstepClip = footStepGroundClip;
+                break;
+            case "Metal": //Metal
+                footstepClip = footStepMetalicClip;
+                break;
+            case "Madera": //Madera
+                footstepClip = footStepWoodClip;
+                break;
+            case "Concrete": //Cemento
+                footstepClip = footStepConcreteClip;
+                break;
+            case "Sand": //Arena
+                footstepClip = footStepSandClip;
+                break;
+            case "Water": //Agua
+                footstepClip = footStepWaterClip;
+                break;
+
+            default: break;
+
+        }
+
+        if (footstepClip.Length > 0)
+            audioSource.PlayOneShot(footstepClip[footstepClip.Length - 1]);
+
+    }
+
+    public void PlayJumpSound() 
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.volume = audioSource.volume;
+        audioSource.pitch = audioSource.pitch;
+        audioSource.PlayOneShot(jumpClip[jumpClip.Length - 1]);
+
+    }
+
+    public void PlayPushSound() 
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.volume = audioSource.volume;
+        audioSource.pitch = audioSource.pitch;
+        audioSource.PlayOneShot(pushClip[pushClip.Length - 1]);
+
+    }
+
+    public void PlayPainSound() 
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.volume = audioSource.volume;
+        audioSource.pitch = audioSource.pitch;
+        audioSource.PlayOneShot(painClip[painClip.Length - 1]);
+
+    }
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+
+            case "Grass":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Ground":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Metal":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Madera":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Concrete":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Sand":
+                floorMaterial = collision.gameObject.tag;
+                break;
+            case "Water":
+                floorMaterial = collision.gameObject.tag;
+                break;
+
+            default: break;
+
+        }
+    }
+   
+
+}
