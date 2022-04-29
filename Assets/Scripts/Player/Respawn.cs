2b67491310b8b34b1e82ebc3a1a6a2afb2ee36f9
alex <alex.romero@enti.cat>
2022-04-29 16:42:49 +0200

    OriginalChekPoint

--- Assets/Scripts/Player/Respawn.cs
+++ Assets/Scripts/Player/Respawn.cs
@@ -7,11 +7,16 @@ public class Respawn : MonoBehaviour
   private ThirdPersonMovement thirdPersonMovement;
   private GameObject Player;
   public Transform respawnPoint;
+    private Transform OriginRespawn;
+
 
     private void Start()
     {
+
+
         Player = GameObject.Find("Player");
         thirdPersonMovement = Player.GetComponent<ThirdPersonMovement>();
+        OriginRespawn = GameObject.Find("OriginalChekPoint").transform;
 
     }
 
@@ -33,7 +38,7 @@ public class Respawn : MonoBehaviour
         Debug.Log("TELEPORT");
         thirdPersonMovement.disable = true;
         yield return new WaitForSeconds(.1f);
-        Player.transform.position = respawnPoint.transform.position;
+        Player.transform.position = OriginRespawn.position;
         yield return new WaitForSeconds(.1f);
         thirdPersonMovement.disable = false;
         
