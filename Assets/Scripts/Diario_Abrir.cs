df1f3ab0e9a95b3883d5cc48ce7621e7f590a768
Eric Hernandez <eric.hernandez@enti.cat>
2022-04-29 15:50:54 +0200

    Diario abrir con "I", aun no se como salir

---
+++ Assets/Scripts/Diario_Abrir.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diario_Abrir : MonoBehaviour
+{
+    public GameObject Diario;
+    public bool Abierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)){
+            if(Abierto==false){
+                Abierto=true;
+                Time.timeScale = 0;
+                 Cursor.visible = true;
+
+            Diario.SetActive(true);
+            }else{
+                if(Abierto==true){
+                    Abierto=false;
+                    Time.timeScale = 1;
+                    Cursor.visible = false;
+                    Diario.SetActive(false);
+                }
+            
+            }
+        }
+    }   
+}    
