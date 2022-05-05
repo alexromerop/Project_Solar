0064f046b297acdb6ffabf0519113ebae725e459
Eric Hernandez <eric.hernandez@enti.cat>
2022-05-05 22:01:35 +0200

    Escena FInal

---
+++ Assets/End.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+
+    public GameObject Seeu;
+
+    
+      void Start(){
+        
+        StartCoroutine(Endo());
+    }
+
+     public IEnumerator Endo()
+    {
+        yield return new WaitForSeconds(15f);
+        Seeu.SetActive(true);
+        yield return new WaitForSeconds(22f);
+        Application.Quit();
+
+    }
+}
